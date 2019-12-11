4a84634142d847d2993683dce2d9c6c5b05e886b
Nazim Shala <shalanazim@gmail.com>
2019-12-11 12:35:05 +0100

    add files

--- script/tickets.js
+++
@@ -1,9 +0,0 @@
-// import Ticket from './constructor.js' ;
-// let inToDo = [];
-// function tickets(id, text, drag, done) {
-//     id = new Ticket(text, drag , done)
-//     inToDo.push(id)
-// return id
-// }
-// console.log("halllo",inToDo)
-// export default tickets
