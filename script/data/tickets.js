2d18d0f0bc3555b77ae1e94c632353e66d0f8755
Nazim Shala <shalanazim@gmail.com>
2020-03-10 10:35:52 +0100

    Updates

--- script/data/tickets.js
+++ script/data/tickets.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from '../../node_modules/uuid';
+// import { v4 as uuidv4 } from '../../node_modules/uuid';
 import Ticket from './Ticket.js';
 import rerender from './rerender.js';
 
@@ -42,7 +42,7 @@ function openTaskAdder() {
 function submitTicket(event) {
   event.preventDefault();
   if (ticket.value !== "") {
-    tickets.push(new Ticket(`${ticket.value}`, false, "to-do", uuidv4))
+    tickets.push(new Ticket(`${ticket.value}`, false, "to-do", (tickets.length + 1)))
     localStorage.setItem("tickets", JSON.stringify(tickets))
   }
 
