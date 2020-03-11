b833b868ffa80a3fb60e1215df108ed4529d9582
Nazim Shala <shalanazim@gmail.com>
2020-03-11 16:20:06 +0100

    Updates

--- script/data/TicketsList.js
+++ script/data/TicketsList.js
@@ -1,3 +1,7 @@
+import Ticket from './Ticket.js';
+import { v4 as uuidv4 } from '../../node_modules/uuid';
+
+
 class TicketsList {
   constructor() {
     this.tickets = localStorage.getItem("tickets") ? this.tickets = JSON.parse(localStorage.getItem("tickets")) : []
@@ -29,6 +33,32 @@ class TicketsList {
     `
     li.innerHTML = `${innerHtmlTemplate}`;
     return li;
+  }
+
+
+  submitTicket(event, button, ticket, ticketTitle) {
+    let tickets = []
+    event.preventDefault();
+    if (button.innerText === 'Submit') {
+      tickets = this.add(new Ticket(`${ticketTitle.value}`, `${ticket.value}`, false, "to-do", uuidv4));
+      this.updateList(tickets);
+    } else {
+      tickets = this.getList()
+      tickets.forEach(element => {
+        if (element.id === button.setDataId) {
+          element.title = ticketTitle.value;
+          element.text = ticket.value;
+        }
+      });
+      this.updateList(tickets);
+    }
+
+    ticket.value = "";
+    ticketTitle.value = "";
+    taskAdder.style.display = "none";
+    button.disabled = true;
+    button.innerText = 'Submit';
+    button.classList.remove("primary");
   }
 }
 
--- script/data/tickets.js
+++ script/data/tickets.js
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from '../../node_modules/uuid';
-import Ticket from './Ticket.js';
 import TicketsList from './TicketsList.js';
 import { rerender, TicketList } from './rerender.js';
 
@@ -9,63 +7,35 @@ if (!TicketList) {
 }
 
 const addTicket = document.querySelector("#addTask");
-const submitTask = document.querySelector("#submit");
+const button = document.querySelector("#submit");
 const cancelTask = document.querySelector("#cancle");
 
-let tickets = []
 let taskAdder = document.querySelector("#taskAdder");
 let ticket = document.querySelector("textarea");
 let ticketTitle = document.querySelector("#title");
 
 ticketTitle.addEventListener("input", () => {
   if (ticketTitle.value.trim() !== "") {
-    submitTask.disabled = false;
-    submitTask.classList.add("primary");
+    button.disabled = false;
+    button.classList.add("primary");
   } else {
-    submitTask.disabled = true;
-    submitTask.classList.remove("primary");
+    button.disabled = true;
+    button.classList.remove("primary");
   }
 }, false)
 
-addTicket.addEventListener("click", openTaskAdder, false);
-submitTask.addEventListener("click", submitTicket, false);
-cancelTask.addEventListener("click", (e) => {
-  e.preventDefault();
-  ticketTitle.value = "";
-  ticket.value = "";
-  submitTask.disabled = true;
-  taskAdder.style.display = "none";
+addTicket.addEventListener("click", () => taskAdder.style.display = "flex", false);
+
+button.addEventListener("click", () => {
+  TicketList.submitTicket(event, button, ticket, ticketTitle);
+  rerender();
 }, false);
 
 
-function openTaskAdder() {
-  taskAdder.style.display = "flex";
-}
-
-function submitTicket(event) {
+cancelTask.addEventListener("click", (event) => {
   event.preventDefault();
-  if (submitTask.innerText === 'Submit') {
-    tickets = TicketList.add(new Ticket(`${ticketTitle.value}`, `${ticket.value}`, false, "to-do", uuidv4));
-    TicketList.updateList(tickets);
-  } else {
-    let ticketIndex = submitTask.innerText.split(" ")[submitTask.innerText.split(" ").length - 1];
-    tickets = TicketList.getList()
-    tickets.forEach(element => {
-      if (element.id === ticketIndex) {
-        element.title = ticketTitle.value;
-        element.text = ticket.value;
-      }
-    });
-    TicketList.updateList(tickets);
-  }
-
-  ticket.value = "";
   ticketTitle.value = "";
+  ticket.value = "";
+  button.disabled = true;
   taskAdder.style.display = "none";
-  submitTask.disabled = true;
-  submitTask.innerText = 'Submit';
-  submitTask.classList.remove("primary");
-  rerender();
-}
-
-export { tickets }
+}, false);
